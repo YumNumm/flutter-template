f0d530b3d85d63ceed270be00b0312bdb92cc23b
YumNumm <admin@yumnumm.dev>
2025-11-20 03:37:18 +0900

    add: iOS, Androidの構成
    Signed-off-by: YumNumm <admin@yumnumm.dev>

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -1,8 +1,25 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val dartDefines = mutableMapOf<String, String>()
+if (project.hasProperty("dart-defines")) {
+  val defines = project.property("dart-defines") as String
+  defines.split(",").forEach { entry ->
+    val decoded = String(Base64.getDecoder().decode(entry))
+    val pair = decoded.split("=")
+    if (pair.size == 2) {
+      dartDefines[pair[0]] = pair[1]
+    }
+  }
+}
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -20,21 +37,35 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "net.yumnumm.template"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        dartDefines["APP_ID_SUFFIX"]?.let {
+            applicationIdSuffix = it
+        }
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        resValue("string", "APP_NAME", dartDefines["APP_NAME"] ?: "template")
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = (keystoreProperties["storeFile"] as String?)?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            multiDexEnabled = true
+        }
+        getByName("debug") {
+            versionNameSuffix = ".d"
         }
     }
 }
